52e47edc8b49d3ef74e0e7f8ae953f9be25cfe20
Lucas Benelli <1Lucasbenelli@gmail.com>
2026-08-10 15:18:29 -0300

    feat(frontend): implementação de possibilidades de paleta de cores para o front

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,8 @@ import './styles/variables.css'
 import './styles/global.css'
 import App from './App.jsx'
 
+document.documentElement.dataset.theme = 'sage'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
